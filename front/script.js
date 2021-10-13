4e127503291a6dacc3fb2dc5ec00d73518e77a37
Viloshe87 <ourliac.wilfried@gmail.com>
2021-10-13 18:20:10 +0100

    ajout card javascript

--- front/script.js
+++ front/script.js
@@ -1,23 +1,35 @@
-
-//  Récupération des données pour les produits (canapés) avec l'API fetch.
+//fetch de l'URL
 fetch('http://localhost:3000/api/products')
-  //Demande de récupération de requête format json
-  .then(function (res) {
-    if (res.ok) {
-      return res.json();
+    .then((response) => response.json())
+    .then((data) => {
+        addCards(data);
+    })
+    .catch((erreur) => console.log("erreur : " + erreur));
+
+// fonction pour la création des cards de la page d'accueil
+function addCards(data) {
+    //boucle pour chaque iteration d'un produit
+    for (produit of data) {
+        //recupère l'élément liste dans le HTML
+        const card = document.getElementById("items");
+      
+        card.innerHTML += `
+        <a href="./product.html?id=42">
+        <article>
+          <img src="" alt="Lorem ipsum dolor sit amet, Kanap name1">
+          <h3 class="productName">Kanap name1</h3>
+          <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada
+            risus sapien gravida nulla nisl arcu.</p>
+     
+          </article>
+      </a> `;
     }
-  })
-  .then(function (products) {
-    console.log(products);
-    
-
-
-  })
+}
 
 
 
 
-
+  
 
 
 
