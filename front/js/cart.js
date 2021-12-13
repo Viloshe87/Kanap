62b7a97bf6e5435e5d5d6178a02c2c360eabb626
Viloshe87 <ourliac.wilfried@gmail.com>
2021-12-13 11:20:44 +0100

    ajout formulaire de contact

--- front/js/cart.js
+++ front/js/cart.js
@@ -14,116 +14,141 @@ if (isCartempty()) {
             displayQuantity(quantity);
             displayProducts(products);
             listenForDeletion();
+            listenForQtyChange();
 
         })
         .catch((erreur) => console.log("erreur :" + erreur));
 }
 
-function listenForDeletion() {
-    document.querySelectorAll('deleteItem').forEach(button => {
 
-        button.addEventListener('click', (e) => {
-            let target = e.target;
-            let id = target.getAttribute('data.id');
-            let color = target.getAttribute('data.color');
+// function listenForQtyChange() {
+//     document.querySelectorAll('.itemQuantity').forEach(button => {
 
-            let productsInInCart = JSON.parse(localStorage.getItem("products"));
-            let index = productsInInCart.findIndex(product => product.id === id && product.color == color);
-            productsInInCart.splice(index, 1);
+//         button.addEventListener('change', (e) => {
+//             let target = e.target;
+//             let id = target.getAttribute('data.id');
+//             let color = target.getAttribute('data.color');
+//             let qty = target.value;
 
-            localStorage.setItem('products', JSON.stringnify(productsInInCart))
-            location.reload();
+//             if (qty < 1) {
+//                 alert('Merci de mettre une quantité supérieur a 0, sinon supprimer le produit');
+//                 productsInCart[index].qty = qty;
+//             }
+//             let productsInCart = JSON.parse(localStorage.getItem("products"));
+//             let index = productsInCart.findIndex(product.id === id && product.color == color);
+//             productsInCart[index].qty = qty;
+//             localStorage.setItem('products', JSON.stringify(productsInCart))
+//             loacation.reload();
+//         })
+//     })
+
+    function listenForDeletion() {
+        document.querySelectorAll('.deleteItem').forEach(button => {
+
+            button.addEventListener('click', (e) => {
+                let target = e.target;
+                let id = target.getAttribute('data-id');
+                let color = target.getAttribute('data-color');
+
+                let productsInCart = JSON.parse(localStorage.getItem("products"));
+                let index = productsInCart.findIndex(product => product.id === id && product.color == color);
+                productsInCart.splice(index, 1);
+
+                localStorage.setItem('products', JSON.stringify(productsInCart))
+                location.reload();
+            })
         })
-    })
-}
+    }
 
-function displayProducts(products) {
-    let html = '';
+    function displayProducts(products) {
+        let html = '';
 
-    products.forEach(product => {
-        html += render(product);
-    })
-    document.getElementById('cart__items').innerHTML = html
-}
-//Récupération des infos de la page produit
-function countTotal(products) {
+        products.forEach(product => {
+            html += render(product);
+        })
+        document.getElementById('cart__items').innerHTML = html
+    }
 
-    let total = 0;
-    products.forEach(product => {
-        total = total + (Number(product.price) * Number(product.qty))
-    })
-    return total;
-}
 
-function countQuantity(products) {
-    let quantity = 0;
+    //Récupération des infos de la page produit
+    function countTotal(products) {
 
-    products.forEach(product => {
-        quantity = quantity + Number(product.qty);
-    })
-    return quantity;
-}
+        let total = 0;
+        products.forEach(product => {
+            total = total + (Number(product.price) * Number(product.qty))
+        })
+        return total;
+    }
 
-function render(product) {
-    return `<article class="cart__item" data-id="${product._id}">
+    function countQuantity(products) {
+        let quantity = 0;
+
+        products.forEach(product => {
+            quantity = quantity + Number(product.qty);
+        })
+        return quantity;
+    }
+
+    function render(product) {
+        return `<article class="cart__item" data-id="${product._id}">
     <div class="cart__item__img">
       <img src="${product.imageUrl}" alt="Photographie d'un canapé">
     </div>
     <div class="cart__item__content">
       <div class="cart__item__content__titlePrice">
-        <h2>'${product.name}' - '${product.color}'</h2>
-        <p>"${Formatter.format(product.price)}"</p>
+        <h2>${product.name}- ${product.color}</h2>
+        <p>${formatter.format(product.price)}</p>
       </div>
       <div class="cart__item__content__settings">
         <div class="cart__item__content__settings__quantity">
           <p>Qté : </p>
-          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.qty}">
+          <input type="number" class="itemQuantity" data.id="${product._id}" data.color="${product.color} " name="itemQuantity" min="1" max="100" value="${product.qty}">
         </div>
         <div class="cart__item__content__settings__delete">
-          <p class="deleteItem" data.id="${product.__id}" data.color="${product.color}" >Supprimer</p>
+          <p class="deleteItem" data-id="${product._id}" data-color="${product.color}" >Supprimer</p>
         </div>
       </div>
     </div>
   </article>`;
-}
-
-function buildCompletProductsList(allProducts) {
-
-    let products = [];
-    let productsInInCart = JSON.parse(localStorage.getItem("products"));
-
-    productsInInCart.forEach(cartItem => {
-        let item = allProducts.find(item => item._id == cartItem.id)
-        let product = { ...item }
-        product.qty = cartItem.qty
-        product.color = cartItem.color
-        console.log(product)
-        products.push(product);
-    })
-    return products;
-}
-
-function isCartempty() {
-    if (!localStorage.getItem('products')) {
-        return true;
     }
-    if (JSON.parse(localStorage.getItem('products')).length == 0) {
-        return true;
-    };
-    return false;
-}
 
-function showemptyCart() {
-    document.querySelector('h1').innerText = 'Panier vide'
-    document.querySelector('.cart').getElementsByClassName.display = 'none'
-}
-//Affichage du prix total du produit
-function displayTotal(total) {
+    function buildCompletProductsList(allProducts) {
 
-    document.getElementById('totalPrice').innerHTML = formatter.format(total)
-}
-//Affichafe de la quantité du produit
-function displayQuantity(quantity) {
+        let products = [];
+        let productsInInCart = JSON.parse(localStorage.getItem("products"));
 
-    document.getElementById('totalQuantity').innerHTML = quantity
-}
+        productsInInCart.forEach(cartItem => {
+            let item = allProducts.find(item => item._id == cartItem.id)
+            let product = { ...item }
+            product.qty = cartItem.qty
+            product.color = cartItem.color
+            console.log(product)
+            products.push(product);
+        })
+        return products;
+    }
+
+    function isCartempty() {
+        if (!localStorage.getItem('products')) {
+            return true;
+        }
+        if (JSON.parse(localStorage.getItem('products')).length == 0) {
+            return true;
+        };
+        return false;
+    }
+
+    function showemptyCart() {
+        document.querySelector('h1').innerText = 'Panier vide'
+        document.querySelector('.cart').getElementsByClassName.display = 'none'
+    }
+    //Affichage du prix total du produit
+    function displayTotal(total) {
+
+        document.getElementById('totalPrice').innerHTML = formatter.format(total)
+    }
+    //Affichafe de la quantité du produit
+    function displayQuantity(quantity) {
+
+        document.getElementById('totalQuantity').innerHTML = quantity
+    }
